4b0c2d3149ea72842192ed9c9831d28d7d204f79
Pradeep Dayaram <vickdayaram@gmail.com>
2017-03-24 14:38:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves){
+  var length = dwarves.length;
+  var dwarvesList = [];
+  for(var i = 0; i < length; i++){
+    dwarvesList.push((i + 1) + ". " + dwarves[i] + " ");
+  }
+return dwarvesList.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var length = planeteerCalls.length;
+  var loudPlaneteer = [];
+  for(var i = 0; i < length; i++){
+    loudPlaneteer.push(planeteerCalls[i].toUpperCase() + "!!!");
+  }
+return loudPlaneteer;
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(words){
+  var length = words.length;
+  for(var i = 0; i < length; i++){
+    if(words[i].length > 4){
+      return true;
+    }
+  }
+return false;
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods){
+  var length = foods.length;
+  for(var i = 0; i < length; i++){
+    if(foods[i] === "cheddar"){
+     return "cheddar";
+    }
+    if(foods[i] === "gouda"){
+     return "gouda";
+    }
+    if(foods[i] === "camembert"){
+     return "camembert";
+    }
+  }
+return "no cheese!";
 }
